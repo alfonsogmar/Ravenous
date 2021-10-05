3dd55cafc4ca09f1869ac04813cc03d647c59627
Alfonso <alfonsogmar@gmail.com>
2021-10-05 10:41:32 +0200

    Creado proyecto Ravenous con componentes estáticos

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
+
+//      <SearchBar />
+//      <BusinessList />
